aef88c87aab6f261494aca4c3d95c4393be8cd7d
jia1105d <150815944+jia1105d@users.noreply.github.com>
2024-07-28 18:05:00 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,17 @@
+// Your web app's Firebase configuration
+//ADD YOUR FIREBASE LINKS HERE
+
+
+// Initialize Firebase
+
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "chat_room.html";
+}
+
+
+
